62225a2314d55defc983232b5bf7fdc27f4e8e80
afroneth <knjh30@gmail.com>
2021-05-25 17:47:35 +0800

    Add individual user page and user list

--- src/App.js
+++ src/App.js
@@ -9,6 +9,8 @@ import { PostsList } from './features/posts/PostsList'
 import { AddPostForm } from './features/posts/AddPostForm'
 import { SinglePostPage } from './features/posts/SinglePostPage'
 import { EditPostForm } from './features/posts/EditPostForm'
+import { UsersList } from './features/users/UsersList'
+import { UserPage } from './features/users/UserPage'
 
 import { Navbar } from './app/Navbar'
 
@@ -30,6 +32,8 @@ function App() {
           />
           <Route exact path="/posts/:postId" component={SinglePostPage} />
           <Route exact path="/editPost/:postId" component={EditPostForm} />
+          <Route exact path="/users" component={UsersList} />
+          <Route exact path="/users/:userId" component={UserPage} />
           <Redirect to="/" />
         </Switch>
       </div>
